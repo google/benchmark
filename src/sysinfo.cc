c44662e7e927ba044ab76d6e3ba3eda50d8e84b8
Dominic Hamon <dominic+github@google.com>
2014-01-15 16:09:18 -0800

    Missing headers for sysctl stuff on OSX

--- src/sysinfo.cc
+++ src/sysinfo.cc
@@ -21,7 +21,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/resource.h>
+#include <sys/sysctl.h>
 #include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include <iostream>
