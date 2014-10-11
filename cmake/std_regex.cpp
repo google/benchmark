e6107a781c35f5e0a366e526631ea8cef8f246c5
Dominic Hamon <dma+github@stripysock.com>
2014-10-11 15:52:50 -0700

    Fix #50 by using nullptr and adding stricter warning.

--- cmake/std_regex.cpp
+++ cmake/std_regex.cpp
@@ -2,7 +2,9 @@
 #include <string>
 int main() {
   const std::string str = "test0159";
-  const std::regex re("^[a-z]+[0-9]+$", std::regex_constants::extended | std::regex_constants::nosubs);
+  const std::regex re(
+      "^[a-z]+[0-9]+$",
+      std::regex_constants::extended | std::regex_constants::nosubs);
   return std::regex_search(str, re) ? 0 : -1;
 }
 
