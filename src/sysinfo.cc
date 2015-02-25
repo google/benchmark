8e3e78fc3dcae25f54dffb1cccb34c0415cee36b
Pavel Davydov <pdavydov108@gmail.com>
2015-02-25 10:41:26 +0300

    Fix #77 add comment to headers in wrong order

--- src/sysinfo.cc
+++ src/sysinfo.cc
@@ -21,7 +21,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/resource.h>
-#include <sys/types.h>
+#include <sys/types.h> // this header must be included before 'sys/sysctl.h' to avoid compilation error on FreeBSD
 #include <sys/sysctl.h>
 #include <sys/time.h>
 #include <unistd.h>
