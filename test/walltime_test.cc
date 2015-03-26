21207a4df5ae471f46ef52598c701dd927e6e5b3
Eric Fiselier <eric@efcs.ca>
2015-03-26 15:25:08 -0400

    improve walltime test

--- test/walltime_test.cc
+++ test/walltime_test.cc
@@ -5,16 +5,28 @@
 #include "../src/walltime.h"
 
 void BM_CPUTimeNow(benchmark::State& state) {
+    using benchmark::walltime::CPUWalltimeNow;
     while (state.KeepRunning()) {
-        benchmark::WallTime volatile now = benchmark::walltime::CPUWalltimeNow();
+        benchmark::WallTime volatile now;
+        now = CPUWalltimeNow();
+        now = CPUWalltimeNow();
+        now = CPUWalltimeNow();
+        now = CPUWalltimeNow();
+        now = CPUWalltimeNow();
         ((void)now);
     }
 }
 BENCHMARK(BM_CPUTimeNow);
 
 void BM_ChronoTimeNow(benchmark::State& state) {
+    using benchmark::walltime::ChronoWalltimeNow;
     while (state.KeepRunning()) {
-        benchmark::WallTime volatile now = benchmark::walltime::ChronoWalltimeNow();
+        benchmark::WallTime volatile now;
+        now = ChronoWalltimeNow();
+        now = ChronoWalltimeNow();
+        now = ChronoWalltimeNow();
+        now = ChronoWalltimeNow();
+        now = ChronoWalltimeNow();
         ((void)now);
     }
 }
