c48a09417bd171aadfdcbd25d76a3fc78d73334f
Eric Fiselier <eric@efcs.ca>
2016-08-02 19:08:19 -0600

    Add compare_bench.py script for comparing benchmarks

--- tools/compare_bench.py
+++ tools/compare_bench.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-
+"""
+compare_bench.py - Compare two benchmarks or their results and report the
+                   difference.
+"""
 import sys
 import gbench
-import gbench.report as greport
-import gbench.util as gutil
+from gbench import util, report
 
 def main():
     # Parse the command line flags
@@ -18,7 +20,10 @@ def main():
     # Run the benchmarks and report the results
     json1 = gbench.util.run_or_load_benchmark(tests[0], bench_opts)
     json2 = gbench.util.run_or_load_benchmark(tests[1], bench_opts)
-    gbench.report.report_difference(json1, json2)
+    output_lines = gbench.report.generate_difference_report(json1, json2)
+    print 'Comparing %s to %s' % (tests[0], tests[1])
+    for ln in output_lines:
+        print(ln)
 
 
 if __name__ == '__main__':
