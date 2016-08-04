81b5f5fb365e2e918b36165a6850c807dd3a08ca
jppm <joaoppmagalhaes@gmail.com>
2016-08-04 02:24:14 +0100

    Update fixture_test to the new Fixture interface.

--- test/fixture_test.cc
+++ test/fixture_test.cc
@@ -6,14 +6,14 @@
 
 class MyFixture : public ::benchmark::Fixture {
  public:
-  void SetUp(const ::benchmark::State& state) {
+  void SetUp(::benchmark::State& state) {
     if (state.thread_index == 0) {
       assert(data.get() == nullptr);
       data.reset(new int(42));
     }
   }
 
-  void TearDown(const ::benchmark::State& state) {
+  void TearDown(::benchmark::State& state) {
     if (state.thread_index == 0) {
       assert(data.get() != nullptr);
       data.reset();
@@ -53,33 +53,50 @@ BENCHMARK_REGISTER_F(MyFixture, Bar)->Arg(42)->ThreadPerCpu();
 class CounterFixture : public ::benchmark::Fixture {
   public:
 
-  void InitState(benchmark::State &st) {
+  void SetUp(benchmark::State &st) override {
+    // add one counter at the beginning
     posFoo = st.counters.Insert("Foo");
+    // add more counters to make lookup more expensive
+    char buf[64];
+    for(int i = 1; i < st.range_x() - 1; ++i) {
+      snprintf(buf, sizeof(buf), "Pad%d", i);
+      st.counters.Insert(buf);
+    }
+    // add one counter at the end
     posBar = st.counters.Insert("Bar");
+    st.counters.Insert("Baz");
   }
 
-  size_t posFoo, posBar;
+  size_t posFoo = -1, posBar = -1;
 };
 BENCHMARK_DEFINE_F(CounterFixture, BumpById)(benchmark::State& st) {
-  assert(posFoo == 0);
-  assert(posBar == 1);
+  assert(posFoo != size_t(-1));
+  assert(posBar != size_t(-1));
   while (st.KeepRunning()) {
-      st.counters[posFoo] += 1.;
-      st.counters[posBar] += 1.;
+    st.counters[posFoo] += 1.;
+    st.counters[posBar] += 1.;
   }
   assert(st.counters[posFoo].Value() > 0.);
+  assert(st.counters[posBar].Value() > 0.);
   st.SetItemsProcessed(st.iterations());
 }
-BENCHMARK_REGISTER_F(CounterFixture, BumpById);
-
 BENCHMARK_DEFINE_F(CounterFixture, BumpByName)(benchmark::State& st) {
+  assert(posFoo != size_t(-1));
+  assert(posBar != size_t(-1));
   while (st.KeepRunning()) {
-      st.counters["Foo"] += 1.;
-      st.counters["Bar"] += 1.;
+    st.counters["Foo"] += 1.;
+    st.counters["Bar"] += 1.;
   }
   assert(st.counters[posFoo].Value() > 0.);
+  assert(st.counters[posBar].Value() > 0.);
   st.SetItemsProcessed(st.iterations());
 }
-BENCHMARK_REGISTER_F(CounterFixture, BumpByName);
+
+BENCHMARK_REGISTER_F(CounterFixture, BumpById)->Arg(2);
+BENCHMARK_REGISTER_F(CounterFixture, BumpByName)->Arg(2);
+BENCHMARK_REGISTER_F(CounterFixture, BumpById)->Arg(16);
+BENCHMARK_REGISTER_F(CounterFixture, BumpByName)->Arg(16);
+BENCHMARK_REGISTER_F(CounterFixture, BumpById)->Arg(64);
+BENCHMARK_REGISTER_F(CounterFixture, BumpByName)->Arg(64);
 
 BENCHMARK_MAIN()
