ef2df61fc642a791c98e4f066f0cf05104fd667e
Eric Fiselier <eric@efcs.ca>
2016-08-07 22:04:15 -0600

    final test

--- .travis-libcxx-setup.sh
+++ .travis-libcxx-setup.sh
@@ -10,10 +10,11 @@ git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm-source
 git clone --depth=1 https://github.com/llvm-mirror/libcxx.git llvm-source/projects/libcxx
 git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm-source/projects/libcxxabi
 
-# Build and install libc++
+# Build and install libc++ (Use unstable ABI for better sanitizer coverage)
 mkdir llvm-build && cd llvm-build
 cmake -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${COMPILER} \
       -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr \
+      -DLIBCXX_ABI_UNSTABLE=ON \
       -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER} \
       ../llvm-source
 make cxx -j2
