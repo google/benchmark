94cde24d964e37ce2c54321f7957008663526efb
Eric Fiselier <eric@efcs.ca>
2016-08-07 21:29:08 -0600

    try without link flags

--- .travis-libcxx-setup.sh
+++ .travis-libcxx-setup.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+# Install a newer CMake version
 curl -sSL https://cmake.org/files/v3.6/cmake-3.6.1-Linux-x86_64.sh -o install-cmake.sh
 chmod +x install-cmake.sh
 sudo ./install-cmake.sh --prefix=/usr/local --skip-license
+
+# Checkout LLVM sources
 git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm-source
 cd llvm-source/projects
 git clone --depth=1 https://github.com/llvm-mirror/libcxx.git
 git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git
 cd ../.. && mkdir llvm-build && cd llvm-build
-cmake -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${COMPILER} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DLLVM_USE_SANITIZER=${LLVM_SAN} ../llvm-source
+cmake -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${COMPILER} \
+      -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr \
+      -DLLVM_USE_SANITIZER=${LLVM_SAN} \
+      ../llvm-source
 make cxx
 sudo make install-libcxxabi install-libcxx
 cd ../
