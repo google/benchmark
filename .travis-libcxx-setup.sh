e4ead51c404281fddf39ce5a3f2d7d602a813779
Eric Fiselier <eric@efcs.ca>
2016-08-07 20:46:47 -0600

    try 4

--- .travis-libcxx-setup.sh
+++ .travis-libcxx-setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+curl -sSL https://cmake.org/files/v3.6/cmake-3.6.1-Linux-x86_64.sh -o install-cmake.sh
+chmod +x install-cmake.sh
+sudo ./install-cmake.sh --prefix=/usr/local --skip-license
 git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm-source
 cd llvm-source/projects
 git clone --depth=1 https://github.com/llvm-mirror/libcxx.git
