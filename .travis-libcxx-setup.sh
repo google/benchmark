db376c8654c18701c111f7e789f9f2bd0ec8d689
Eric Fiselier <eric@efcs.ca>
2016-09-03 01:45:59 -0600

    Tweak TSAN flags

--- .travis-libcxx-setup.sh
+++ .travis-libcxx-setup.sh
@@ -13,7 +13,7 @@ git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm-source/pro
 # Build and install libc++ (Use unstable ABI for better sanitizer coverage)
 mkdir llvm-build && cd llvm-build
 cmake -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${COMPILER} \
-      -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr \
       -DLIBCXX_ABI_UNSTABLE=ON \
       -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER} \
       ../llvm-source
