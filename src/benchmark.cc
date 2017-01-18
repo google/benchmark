efbcc60538ccb6cfeefc39911661f874ce8e3498
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-01-18 04:10:08 +0100

    make ReportUnrecognizedArguments() print to stderr

--- src/benchmark.cc
+++ src/benchmark.cc
@@ -666,7 +666,7 @@ void Initialize(int* argc, char** argv) {
 
 void ReportUnrecognizedArguments(int argc, char** argv) {
   for (int i = 1; i < argc; ++i) {
-    printf("%s: error: unrecognized command-line flag: %s\n", argv[0], argv[i]);
+    fprintf(stderr, "%s: error: unrecognized command-line flag: %s\n", argv[0], argv[i]);
   }
 }
 
