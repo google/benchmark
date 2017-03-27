7e305b0c203c24becb7c17ce38197250edb514b2
Eric Fiselier <eric@efcs.ca>
2017-03-27 17:57:20 -0600

    Replace int64_t usages with 'int' instead.
    Previously the constants used for converting between
    different units of time were declared using int64_t. However
    we should only use explicitly sized integer types when they
    are required, and should use 'int' everwhere else, and there is
    no good reason to use int64_t here.
    
    For that reason this patch changes the type of the constants.
    This should help address issue #354 as well.

--- src/sleep.cc
+++ src/sleep.cc
@@ -41,7 +41,7 @@ void SleepForMicroseconds(int microseconds) {
 }
 
 void SleepForMilliseconds(int milliseconds) {
-  SleepForMicroseconds(static_cast<int>(milliseconds) * kNumMicrosPerMilli);
+  SleepForMicroseconds(milliseconds * kNumMicrosPerMilli);
 }
 
 void SleepForSeconds(double seconds) {
