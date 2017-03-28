a43641a55022f624f182c7bb550c320fc5e425dc
Eric Fiselier <eric@efcs.ca>
2017-03-27 19:43:28 -0600

    Attempt to fix libc++ travis build w/ 32 bits

--- .travis-libcxx-setup.sh
+++ .travis-libcxx-setup.sh
@@ -12,7 +12,7 @@ git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm-source/pro
 
 # Setup libc++ options
 if [ -z "$BUILD_32_BITS" ]; then
-  export BUILD_32_BITS=OFF
+  export BUILD_32_BITS=OFF && echo disabling 32 bit build
 fi
 
 # Build and install libc++ (Use unstable ABI for better sanitizer coverage)
@@ -21,8 +21,7 @@ cmake -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${COMPILER} \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr \
       -DLIBCXX_ABI_UNSTABLE=ON \
       -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER} \
-      -DLIBCXX_BUILD_32_BITS=${BUILD_32_BITS} \
-      -DLIBCXXABI_BUILD_32_BITS=${BUILD_32_BITS} \
+      -DLLVM_BUILD_32_BITS=${BUILD_32_BITS} \
       ../llvm-source
 make cxx -j2
 sudo make install-cxxabi install-cxx
