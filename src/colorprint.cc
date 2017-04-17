9085b35d941dd2c81d77ee461f98fafb1567b68d
Dmitry Trifonov <dmitry.trifonov@hp.com>
2017-04-17 16:28:57 -0700

    using vsnprintf instead of std::vsnprintf to compile on Android

--- src/colorprint.cc
+++ src/colorprint.cc
@@ -89,11 +89,7 @@ std::string FormatString(const char* msg, va_list args) {
 
   std::size_t size = 256;
   char local_buff[256];
-#if defined(__ANDROID__) && (defined(__GLIBCXX__) || defined(__GLIBCPP__))
-  auto ret = ::vsnprintf(local_buff, size, msg, args_cp);
-#else 
-  auto ret = std::vsnprintf(local_buff, size, msg, args_cp);
-#endif
+  auto ret = vsnprintf(local_buff, size, msg, args_cp);
 
   va_end(args_cp);
 
@@ -108,11 +104,7 @@ std::string FormatString(const char* msg, va_list args) {
     // we did not provide a long enough buffer on our first attempt.
     size = (size_t)ret + 1;  // + 1 for the null byte
     std::unique_ptr<char[]> buff(new char[size]);
-#if defined(__ANDROID__) && (defined(__GLIBCXX__) || defined(__GLIBCPP__))
-    ret = ::vsnprintf(buff.get(), size, msg, args);
-#else 
-    ret = std::vsnprintf(buff.get(), size, msg, args);
-#endif
+    ret = vsnprintf(buff.get(), size, msg, args);
     CHECK(ret > 0 && ((size_t)ret) < size);
     return buff.get();
   }
