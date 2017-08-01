3347a20e0e7aff733371cdc2ec371ea73bfdcecb
Roman Lebedev <lebedev.ri@gmail.com>
2017-08-01 04:04:02 +0300

    reporter_output_test: json: iterations is int, not float (#431)
    May be relevant for flakiness of win builds
    
    Noted by @KindDragon

--- test/user_counters_tabular_test.cc
+++ test/user_counters_tabular_test.cc
@@ -68,7 +68,7 @@ void BM_Counters_Tabular(benchmark::State& state) {
 }
 BENCHMARK(BM_Counters_Tabular)->ThreadRange(1, 16);
 ADD_CASES(TC_JSONOut, {{"\"name\": \"BM_Counters_Tabular/threads:%int\",$"},
-                       {"\"iterations\": %float,$", MR_Next},
+                       {"\"iterations\": %int,$", MR_Next},
                        {"\"real_time\": %float,$", MR_Next},
                        {"\"cpu_time\": %float,$", MR_Next},
                        {"\"time_unit\": \"ns\",$", MR_Next},
@@ -112,7 +112,7 @@ void BM_CounterRates_Tabular(benchmark::State& state) {
 }
 BENCHMARK(BM_CounterRates_Tabular)->ThreadRange(1, 16);
 ADD_CASES(TC_JSONOut, {{"\"name\": \"BM_CounterRates_Tabular/threads:%int\",$"},
-                       {"\"iterations\": %float,$", MR_Next},
+                       {"\"iterations\": %int,$", MR_Next},
                        {"\"real_time\": %float,$", MR_Next},
                        {"\"cpu_time\": %float,$", MR_Next},
                        {"\"time_unit\": \"ns\",$", MR_Next},
@@ -156,7 +156,7 @@ void BM_CounterSet0_Tabular(benchmark::State& state) {
 }
 BENCHMARK(BM_CounterSet0_Tabular)->ThreadRange(1, 16);
 ADD_CASES(TC_JSONOut, {{"\"name\": \"BM_CounterSet0_Tabular/threads:%int\",$"},
-                       {"\"iterations\": %float,$", MR_Next},
+                       {"\"iterations\": %int,$", MR_Next},
                        {"\"real_time\": %float,$", MR_Next},
                        {"\"cpu_time\": %float,$", MR_Next},
                        {"\"time_unit\": \"ns\",$", MR_Next},
@@ -188,7 +188,7 @@ void BM_CounterSet1_Tabular(benchmark::State& state) {
 }
 BENCHMARK(BM_CounterSet1_Tabular)->ThreadRange(1, 16);
 ADD_CASES(TC_JSONOut, {{"\"name\": \"BM_CounterSet1_Tabular/threads:%int\",$"},
-                       {"\"iterations\": %float,$", MR_Next},
+                       {"\"iterations\": %int,$", MR_Next},
                        {"\"real_time\": %float,$", MR_Next},
                        {"\"cpu_time\": %float,$", MR_Next},
                        {"\"time_unit\": \"ns\",$", MR_Next},
@@ -224,7 +224,7 @@ void BM_CounterSet2_Tabular(benchmark::State& state) {
 }
 BENCHMARK(BM_CounterSet2_Tabular)->ThreadRange(1, 16);
 ADD_CASES(TC_JSONOut, {{"\"name\": \"BM_CounterSet2_Tabular/threads:%int\",$"},
-                       {"\"iterations\": %float,$", MR_Next},
+                       {"\"iterations\": %int,$", MR_Next},
                        {"\"real_time\": %float,$", MR_Next},
                        {"\"cpu_time\": %float,$", MR_Next},
                        {"\"time_unit\": \"ns\",$", MR_Next},
