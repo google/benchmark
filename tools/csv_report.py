0f2816e657cdc65557f1d80aba88d51a1e0b5795
Dominic Hamon <dominic+github@google.com>
2017-09-10 17:52:58 -0700

    Formatting tweaks

--- tools/csv_report.py
+++ tools/csv_report.py
@@ -12,36 +12,36 @@ from gbench.util import *
 
 
 def check_inputs(test, flags):
-    """
-    Perform checking on the user provided inputs and diagnose any abnormalities
-    """
-    test_kind, test_err = classify_input_file(test)
-    output_file = find_benchmark_flag('--benchmark_out=', flags)
-    output_type = find_benchmark_flag('--benchmark_out_format=', flags)
-    if test_kind == IT_Executable and output_file:
-        print(("WARNING: '--benchmark_out=%s' will be passed to "
-              "benchmark causing it to be overwritten") % output_file)
-    if test_kind == IT_JSON and len(flags) > 0:
-        print("WARNING: passing --benchmark flags has no effect since input is "
-              "JSON")
-    if output_type is not None and output_type != 'json':
-        print(("ERROR: passing '--benchmark_out_format=%s' to 'csv_report.py`"
-              " is not supported.") % output_type)
-        sys.exit(1)
+  """
+  Perform checking on the user provided inputs and diagnose any abnormalities
+  """
+  test_kind, test_err = classify_input_file(test)
+  output_file = find_benchmark_flag('--benchmark_out=', flags)
+  output_type = find_benchmark_flag('--benchmark_out_format=', flags)
+  if test_kind == IT_Executable and output_file:
+    print(("WARNING: '--benchmark_out=%s' will be passed to benchmark causing "
+           "it to be overwritten") % output_file)
+  if test_kind == IT_JSON and len(flags) > 0:
+    print("WARNING: passing --benchmark flags has no effect since input is "
+          "JSON")
+  if output_type is not None and output_type != 'json':
+    print(("ERROR: passing '--benchmark_out_format=%s' to 'csv_report.py`"
+      " is not supported.") % output_type)
+    sys.exit(1)
 
 
 def generate_csv_report(json):
-    """
-    Generate a CSV report from a benchmark result in 'json'.
-    """
-    columns = [c for bn in json['benchmarks'] for c in bn.keys()]
-    columns = list(set(columns))
+  """
+  Generate a CSV report from a benchmark result in 'json'.
+  """
+  columns = [c for bn in json['benchmarks'] for c in bn.keys()]
+  columns = list(set(columns))
 
-    output_strs = [','.join(columns)]
+  output_strs = [','.join(columns)]
 
-    for bn in json['benchmarks']:
-        output_strs += [','.join(map(lambda x: "{}".format(bn.get(x, "")), columns))]
-    return output_strs
+  for bn in json['benchmarks']:
+    output_strs += [','.join(map(lambda x: "{}".format(bn.get(x, "")), columns))]
+  return output_strs
 
 
 def main():
@@ -58,17 +58,16 @@ def main():
   # Parse the command line flags
   test = args.test[0]
   if unknown_args:
-      # should never happen
-      print("Unrecognized positional argument arguments: '%s'"
-            % unknown_args)
-      exit(1)
+    # should never happen
+    print("Unrecognized positional argument arguments: '%s'" % unknown_args)
+    sys.exit(1)
   benchmark_options = args.benchmark_options
   check_inputs(test, benchmark_options)
   # Run the benchmarks and report the results
   json = gbench.util.run_or_load_benchmark(test, benchmark_options)
   output_lines = generate_csv_report(json)
   for ln in output_lines:
-      print(ln)
+    print(ln)
 
 
 if __name__ == '__main__':
