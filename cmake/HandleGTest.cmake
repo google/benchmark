3aa409d013c50bba14f47bf286d4ed42260f9e82
Eric Fiselier <eric@efcs.ca>
2017-11-25 14:33:39 -0700

    Add support for GTest based unit tests.
    As Dominic and I have previously discussed, there is some
    need/desire to improve the testing situation in Google Benchmark.
    
    One step to fixing this problem is to make it easier to write
    unit tests by adding support for GTest, which is what this patch does.
    
    By default it looks for an installed version of GTest. However the
    user can specify -DBENCHMARK_BUILD_EXTERNAL_GTEST=ON to instead
    download, build, and use copy of gtest from source. This is
    quite useful when Benchmark is being built in non-standard configurations,
    such as against libc++ or in 32 bit mode.

---
+++ cmake/HandleGTest.cmake
@@ -0,0 +1,47 @@
+include(ExternalProject)
+
+macro(split_list listname)
+  string(REPLACE ";" " " ${listname} "${${listname}}")
+endmacro()
+
+if (NOT BENCHMARK_BUILD_EXTERNAL_GTEST)
+  find_package(GTest REQUIRED)
+else()
+  set(GTEST_FLAGS "")
+  if (BENCHMARK_USE_LIBCXX)
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+      list(APPEND GTEST_FLAGS -stdlib=libc++)
+    else()
+      message(WARNING "Unsupported compiler (${CMAKE_CXX_COMPILER}) when using libc++")
+    endif()
+  endif()
+  if (BENCHMARK_BUILD_32_BITS)
+    list(APPEND GTEST_FLAGS -m32)
+  endif()
+
+  split_list(GTEST_FLAGS)
+  ExternalProject_Add(googletest
+      EXCLUDE_FROM_ALL ON
+      GIT_REPOSITORY https://github.com/google/googletest.git
+      GIT_TAG master
+      PREFIX "${CMAKE_BINARY_DIR}/googletest"
+      INSTALL_DIR "${CMAKE_BINARY_DIR}/googletest"
+      CMAKE_CACHE_ARGS
+        -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+        -DCMAKE_CXX_FLAGS:STRING=${GTEST_FLAGS}
+      )
+
+  ExternalProject_Get_Property(googletest install_dir)
+  add_library(gtest UNKNOWN IMPORTED)
+  add_library(gtest_main UNKNOWN IMPORTED)
+  set_target_properties(gtest PROPERTIES
+    IMPORTED_LOCATION ${install_dir}/lib/libgtest.a
+  )
+  set_target_properties(gtest_main PROPERTIES
+    IMPORTED_LOCATION ${install_dir}/lib/libgtest_main.a
+  )
+  add_dependencies(gtest googletest)
+  add_dependencies(gtest_main googletest)
+  set(GTEST_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/googletest/include)
+  set(GTEST_BOTH_LIBRARIES gtest gtest_main)
+endif()
