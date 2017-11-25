aad55e9c0c9cd00f573f0a95d33620bc09c65d05
Eric Fiselier <eric@efcs.ca>
2017-11-25 15:48:06 -0700

    Attempt to correctly name gtest library on Windows

--- cmake/HandleGTest.cmake
+++ cmake/HandleGTest.cmake
@@ -21,7 +21,10 @@ else()
   if (NOT "${CMAKE_CXX_FLAGS}" STREQUAL "")
     list(APPEND GTEST_FLAGS ${CMAKE_CXX_FLAGS})
   endif()
-
+  string(TOUPPER "${CMAKE_BUILD_TYPE}" GTEST_BUILD_TYPE)
+  if ("${GTEST_BUILD_TYPE}" STREQUAL "COVERAGE")
+    set(GTEST_BUILD_TYPE "DEBUG")
+  endif()
   split_list(GTEST_FLAGS)
   string(REPLACE "-Wzero-as-null-pointer-constant" "" GTEST_FLAGS "${GTEST_FLAGS}")
   ExternalProject_Add(googletest
@@ -31,6 +34,7 @@ else()
       PREFIX "${CMAKE_BINARY_DIR}/googletest"
       INSTALL_DIR "${CMAKE_BINARY_DIR}/googletest"
       CMAKE_CACHE_ARGS
+        -DCMAKE_BUILD_TYPE:STRING=${GTEST_BUILD_TYPE}
         -DCMAKE_C_COMPILER:STRING=${CMAKE_C_COMPILER}
         -DCMAKE_CXX_COMPILER:STRING=${CMAKE_CXX_COMPILER}
         -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
@@ -40,13 +44,20 @@ else()
   ExternalProject_Get_Property(googletest install_dir)
   add_library(gtest UNKNOWN IMPORTED)
   add_library(gtest_main UNKNOWN IMPORTED)
+
+  set(LIB_SUFFIX "${CMAKE_STATIC_LIBRARY_SUFFIX}")
+  set(LIB_PREFIX "${CMAKE_STATIC_LIBRARY_PREFIX}")
+
+  if("${GTEST_BUILD_TYPE}" STREQUAL "DEBUG" AND ${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    set(LIB_SUFFIX "d${CMAKE_STATIC_LIBRARY_SUFFIX}")
+  endif()
   set_target_properties(gtest PROPERTIES
-    IMPORTED_LOCATION ${install_dir}/lib/libgtest.a
+    IMPORTED_LOCATION ${install_dir}/lib/${LIB_PREFIX}gtest${LIB_SUFFIX}
     INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${install_dir}/include
 
   )
   set_target_properties(gtest_main PROPERTIES
-    IMPORTED_LOCATION ${install_dir}/lib/libgtest_main.a
+    IMPORTED_LOCATION ${install_dir}/lib/${LIB_PREFIX}gtest_main${LIB_SUFFIX}
     INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${install_dir}/include
   )
   add_dependencies(gtest googletest)
