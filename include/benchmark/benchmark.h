9cd212f783e368b1bdfee925242c1d66ee9e94df
Sam Panzer <panzer@google.com>
2018-02-12 19:11:00 -0500

    Improve codegen by helping the compiler undestand dead code.

--- include/benchmark/benchmark.h
+++ include/benchmark/benchmark.h
@@ -610,6 +610,9 @@ class State {
 
  private:
   void StartKeepRunning();
+  // Implementation of KeepRunning() and KeepRunningBatch().
+  // is_batch must be true unless n is 1.
+  bool KeepRunningInternal(size_t n, bool is_batch);
   void FinishKeepRunning();
   internal::ThreadTimer* timer_;
   internal::ThreadManager* manager_;
@@ -618,14 +621,21 @@ class State {
 
 inline BENCHMARK_ALWAYS_INLINE
 bool State::KeepRunning() {
-  return KeepRunningBatch(1);
+  return KeepRunningInternal(1, /*is_batch=*/ false);
 }
 
 inline BENCHMARK_ALWAYS_INLINE
 bool State::KeepRunningBatch(size_t n) {
+  return KeepRunningInternal(n, /*is_batch=*/ true);
+}
+
+inline BENCHMARK_ALWAYS_INLINE
+bool State::KeepRunningInternal(size_t n, bool is_batch) {
   // total_iterations_ is set to 0 by the constructor, and always set to a
   // nonzero value by StartKepRunning().
   assert(n > 0);
+  // n must be 1 unless is_batch is true.
+  assert(is_batch || n == 1);
   if (BENCHMARK_BUILTIN_EXPECT(total_iterations_ >= n, true)) {
     total_iterations_ -= n;
     return true;
@@ -637,7 +647,8 @@ bool State::KeepRunningBatch(size_t n) {
       return true;
     }
   }
-  if (total_iterations_ != 0) {
+  // For non-batch runs, total_iterations_ must be 0 by now.
+  if (is_batch && total_iterations_ != 0) {
     batch_leftover_  = n - total_iterations_;
     total_iterations_ = 0;
     return true;
