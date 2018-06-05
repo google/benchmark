9b1650f13182c355e6ef5263925a061947246ae0
Marat Dukhan <maratek@gmail.com>
2018-06-05 03:36:26 -0700

    Fix compilation on Android with GNU STL (#596)
    * Fix compilation on Android with GNU STL
    
    GNU STL in Android NDK lacks string conversion functions from C++11, including std::stoul, std::stoi, and std::stod.
    This patch reimplements these functions in benchmark:: namespace using C-style equivalents from C++03.
    
    * Avoid use of log2 which doesn't exist in Android GNU STL
    
    GNU STL in Android NDK lacks log2 function from C99/C++11.
    This patch replaces their use in the code with double log(double) function.

--- test/complexity_test.cc
+++ test/complexity_test.cc
@@ -134,6 +134,7 @@ static void BM_Complexity_O_N_log_N(benchmark::State& state) {
   }
   state.SetComplexityN(state.range(0));
 }
+static const double kLog2E = 1.44269504088896340736;
 BENCHMARK(BM_Complexity_O_N_log_N)
     ->RangeMultiplier(2)
     ->Range(1 << 10, 1 << 16)
@@ -141,7 +142,7 @@ BENCHMARK(BM_Complexity_O_N_log_N)
 BENCHMARK(BM_Complexity_O_N_log_N)
     ->RangeMultiplier(2)
     ->Range(1 << 10, 1 << 16)
-    ->Complexity([](int64_t n) { return n * log2(n); });
+    ->Complexity([](int64_t n) { return kLog2E * n * log(n); });
 BENCHMARK(BM_Complexity_O_N_log_N)
     ->RangeMultiplier(2)
     ->Range(1 << 10, 1 << 16)
