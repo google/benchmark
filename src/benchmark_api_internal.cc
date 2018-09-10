0c4928fe548bb7d5c2ed87b8ec60ba6655fcd5a5
Dominic Hamon <dominichamon@users.noreply.github.com>
2018-09-10 10:14:23 +0100

    Update benchmark_api_internal.cc

--- src/benchmark_api_internal.cc
+++ src/benchmark_api_internal.cc
@@ -5,10 +5,11 @@
 namespace benchmark {
 namespace internal {
 
-std::unique_ptr<State> BenchmarkInstance::Run(size_t iters, int thread_id,
-                                              internal::ThreadTimer* timer,
-                                              internal::ThreadManager* manager) const {
-  auto st = std::unique_ptr<State>(new State(iters, arg, thread_id, threads, timer, manager));
+std::unique_ptr<State> BenchmarkInstance::Run(
+    size_t iters, int thread_id, internal::ThreadTimer* timer,
+    internal::ThreadManager* manager) const {
+  auto st = std::unique_ptr<State>(new State(iters, arg, thread_id, threads,
+                                             timer, manager));
   benchmark->Run(*st);
   return st;
 }
