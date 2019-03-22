36c76403ab482546e8821f499ba3d67688d99547
Daniel Harvey <danielharvey458@gmail.com>
2019-03-22 05:53:20 +0000

    Add FIXME in multiple_ranges_test.cc

--- test/multiple_ranges_test.cc
+++ test/multiple_ranges_test.cc
@@ -40,6 +40,8 @@ class MultipleRangesFixture : public ::benchmark::Fixture {
   // NOTE: This is not TearDown as we want to check after _all_ runs are
   // complete.
   virtual ~MultipleRangesFixture() {
+     // FIXME Should the 'if' below be checking for equality
+     // of the values in the containers, rather than their size?
     assert(actualValues.size() == expectedValues.size());
     if (actualValues.size() != expectedValues.size()) {
       std::cout << "EXPECTED\n";
