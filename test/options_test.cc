e3666568a90b4f0e72b85252e5a64d9f1bcfca0a
Daniel Harvey <danielharvey458@gmail.com>
2019-03-26 11:50:53 +0100

    Negative ranges #762 (#787)
    * Add FIXME in multiple_ranges_test.cc
    
    * Improve handling of large bounds in AddRange.
    
    Due to breaking the loop too early, AddRange
    would miss a final multplier of 'mult' that
    was within the numeric range of T.
    
    * Enable negative values for Range argument
    
    Fixes #762.
    
    * Try to fix build of benchmark_gtest
    
    * Try some more to fix build
    
    * Attempt to fix format macros
    
    * Attempt to resolve format errors for mingw32
    
    * Review feedback
    
    Put unit tests in benchmark::internal namespace
    
    Fix error reporting in multiple_ranges_test.cc

--- test/options_test.cc
+++ test/options_test.cc
@@ -35,6 +35,16 @@ BENCHMARK(BM_basic)->UseRealTime();
 BENCHMARK(BM_basic)->ThreadRange(2, 4);
 BENCHMARK(BM_basic)->ThreadPerCpu();
 BENCHMARK(BM_basic)->Repetitions(3);
+BENCHMARK(BM_basic)
+    ->RangeMultiplier(std::numeric_limits<int>::max())
+    ->Range(std::numeric_limits<int64_t>::min(),
+            std::numeric_limits<int64_t>::max());
+
+// Negative ranges
+BENCHMARK(BM_basic)->Range(-64, -1);
+BENCHMARK(BM_basic)->RangeMultiplier(4)->Range(-8, 8);
+BENCHMARK(BM_basic)->DenseRange(-2, 2, 1);
+BENCHMARK(BM_basic)->Ranges({{-64, 1}, {-8, -1}});
 
 void CustomArgs(benchmark::internal::Benchmark* b) {
   for (int i = 0; i < 10; ++i) {
