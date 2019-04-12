d38c7a933bee3da552877dade4817669400c3d60
Dominic Hamon <dominic+github@google.com>
2019-04-12 15:49:18 +0100

    Fix typo

--- test/commandlineflags_gtest.cc
+++ test/commandlineflags_gtest.cc
@@ -12,7 +12,7 @@ int setenv(const char* name, const char* value, int overwrite) {
   int errcode = 0;
   if (!overwrite) {
     size_t envsize = 0;
-    errcod = getenv_s(&envsize, nullptr, 0, name);
+    errcode = getenv_s(&envsize, nullptr, 0, name);
     if (errcode || envsize) {
       return errcode;
     }
