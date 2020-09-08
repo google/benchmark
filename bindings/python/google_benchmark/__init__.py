c6f498e21980d5695602447fd89df212acfef4ec
Dominic Hamon <dma+github@stripysock.com>
2020-09-08 17:11:27 +0100

    fix some pylint issues

--- bindings/python/google_benchmark/__init__.py
+++ bindings/python/google_benchmark/__init__.py
@@ -39,27 +39,27 @@ __version__ = "0.1.0"
 
 
 def register(f=None, *, name=None):
-  if f is None:
-    return lambda f: register(f, name=name)
-  if name is None:
-    name = f.__name__
-  _benchmark.RegisterBenchmark(name, f)
-  return f
+    if f is None:
+        return lambda f: register(f, name=name)
+    if name is None:
+        name = f.__name__
+    _benchmark.RegisterBenchmark(name, f)
+    return f
 
 
 def _flags_parser(argv):
-  argv = _benchmark.Initialize(argv)
-  return app.parse_flags_with_usage(argv)
+    argv = _benchmark.Initialize(argv)
+    return app.parse_flags_with_usage(argv)
 
 
 def _run_benchmarks(argv):
-  if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
-  return _benchmark.RunSpecifiedBenchmarks()
+    if len(argv) > 1:
+        raise app.UsageError('Too many command-line arguments.')
+    return _benchmark.RunSpecifiedBenchmarks()
 
 
 def main(argv=None):
-  return app.run(_run_benchmarks, argv=argv, flags_parser=_flags_parser)
+    return app.run(_run_benchmarks, argv=argv, flags_parser=_flags_parser)
 
 
 # Methods for use with custom main function.
