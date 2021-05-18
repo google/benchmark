c069414662203373402450bdbb42bc7f263dad6c
Mircea Trofin <mtrofin@google.com>
2021-05-18 14:49:13 -0700

    const-ed Snapshot

--- src/perf_counters.h
+++ src/perf_counters.h
@@ -92,7 +92,7 @@ class PerfCounters final {
   // Take a snapshot of the current value of the counters into the provided
   // valid PerfCounterValues storage. The values are populated such that:
   // names()[i]'s value is (*values)[i]
-  BENCHMARK_ALWAYS_INLINE bool Snapshot(PerfCounterValues* values) {
+  BENCHMARK_ALWAYS_INLINE bool Snapshot(PerfCounterValues* values) const {
 #ifndef BENCHMARK_OS_WINDOWS
     assert(values != nullptr);
     assert(IsValid());
