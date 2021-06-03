2a4ec99f3f31e60d9c7e62af30c0221a2c3938a1
Dominic Hamon <dma+github@stripysock.com>
2021-06-03 20:34:58 +0100

    new llvm builds differently

--- .github/.libcxx-setup.sh
+++ .github/.libcxx-setup.sh
@@ -9,7 +9,6 @@ if [ -z "$BUILD_32_BITS" ]; then
 fi
 
 # Build and install libc++ (Use unstable ABI for better sanitizer coverage)
-mkdir llvm-build && cd llvm-build
 cmake -DCMAKE_C_COMPILER=${C_COMPILER}          \
       -DCMAKE_CXX_COMPILER=${COMPILER}          \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo         \
@@ -18,6 +17,6 @@ cmake -DCMAKE_C_COMPILER=${C_COMPILER}          \
       -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER}  \
       -DLLVM_BUILD_32_BITS=${BUILD_32_BITS}     \
       -DLLVM_ENABLE_PROJECTS='clang;libcxx;libcxxabi' \
-      ../llvm-project
-make cxx -j2
+      -S llvm -B llvm-build -G "Unix Makefiles"
+make -j2
 sudo make install-libcxxabi install-libcxx install-clang
