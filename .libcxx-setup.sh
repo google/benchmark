493ae94014c1004b042d1b82e0db160f55ddb362
Dominic Hamon <dma+github@stripysock.com>
2021-06-03 11:13:28 +0100

    Enable various sanitizer builds in github actions

--- .libcxx-setup.sh
+++ .libcxx-setup.sh
@@ -19,7 +19,7 @@ fi
 mkdir llvm-build && cd llvm-build
 cmake -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${COMPILER} \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr \
-      -DLIBCXX_ABI_UNSTABLE=ON \
+      -DLIBCXX_ABI_UNSTABLE=OFF \
       -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER} \
       -DLLVM_BUILD_32_BITS=${BUILD_32_BITS} \
       ../llvm-source
