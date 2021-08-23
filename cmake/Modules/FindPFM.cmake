e7fa637cbed5fbad81050014fa166053bbc48c11
Roman Lebedev <lebedev.ri@gmail.com>
2021-08-23 19:36:34 +0300

    [NFC] PFM: actually report package as found, and advertise description
    This slightly prettifies the CMake's `feature_summary()` output,
    should the library be built as part of some project that then prints
    the CMake summary.

--- cmake/Modules/FindPFM.cmake
+++ cmake/Modules/FindPFM.cmake
@@ -5,6 +5,11 @@ include(CheckIncludeFile)
 include(CheckLibraryExists)
 enable_language(C)
 
+set_package_properties(PFM PROPERTIES
+                       URL http://perfmon2.sourceforge.net/
+                       DESCRIPTION "a helper library to develop monitoring tools"
+                       PURPOSE "Used to program specific performance monitoring events")
+
 check_library_exists(libpfm.a pfm_initialize "" HAVE_LIBPFM_INITIALIZE)
 if(HAVE_LIBPFM_INITIALIZE)
   check_include_file(perfmon/perf_event.h HAVE_PERFMON_PERF_EVENT_H)
@@ -13,6 +18,7 @@ if(HAVE_LIBPFM_INITIALIZE)
   if(HAVE_PERFMON_PERF_EVENT_H AND HAVE_PERFMON_PFMLIB_H AND HAVE_PERFMON_PFMLIB_PERF_EVENT_H)
     message("Using Perf Counters.")
     set(HAVE_LIBPFM 1)
+    set(PFM_FOUND 1)
   endif()
 else()
   message("Perf Counters support requested, but was unable to find libpfm.")
