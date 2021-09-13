8f4a5c5bdc78b9887a39289808253e4b837dad55
Vy Nguyen <oontvoo@users.noreply.github.com>
2021-09-13 15:47:42 -0400

    Fix mention of --benchmarks in comment
    This flag doesn't exist ever in OSS benchmark.

--- include/benchmark/benchmark.h
+++ include/benchmark/benchmark.h
@@ -34,7 +34,7 @@ static void BM_StringCopy(benchmark::State& state) {
 BENCHMARK(BM_StringCopy);
 
 // Augment the main() program to invoke benchmarks if specified
-// via the --benchmarks command line flag.  E.g.,
+// via the --benchmark_filter command line flag.  E.g.,
 //       my_unittest --benchmark_filter=all
 //       my_unittest --benchmark_filter=BM_StringCreation
 //       my_unittest --benchmark_filter=String
