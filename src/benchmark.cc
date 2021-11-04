58cecdf771299fa7dfdf5642c78a9f3203e7b644
Bensuperpc <bensuperpc@gmail.com>
2021-11-04 10:05:23 +0100

    Fix typo
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- src/benchmark.cc
+++ src/benchmark.cc
@@ -617,7 +617,9 @@ void Initialize(int* argc, char** argv) {
   internal::LogLevel() = FLAGS_v;
 }
 
-void Shutdown() { delete internal::global_context; }
+void Shutdown() {
+  delete internal::global_context;
+}
 
 bool ReportUnrecognizedArguments(int argc, char** argv) {
   for (int i = 1; i < argc; ++i) {
