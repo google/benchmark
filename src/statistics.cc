74ae5672945e713135a7f47fb26bd14d63488f4f
Dominic Hamon <dominichamon@users.noreply.github.com>
2022-04-07 14:37:22 +0100

    Small optimization to counter map management (#1382)
    * Small optimization to counter map management
    
    Avoids an unnecessary lookup.
    
    * formatting

--- src/statistics.cc
+++ src/statistics.cc
@@ -118,11 +118,13 @@ std::vector<BenchmarkReporter::Run> ComputeStats(
     for (auto const& cnt : r.counters) {
       auto it = counter_stats.find(cnt.first);
       if (it == counter_stats.end()) {
-        counter_stats.insert({cnt.first, {cnt.second, std::vector<double>{}}});
-        it = counter_stats.find(cnt.first);
+        it = counter_stats
+                 .emplace(cnt.first,
+                          CounterStat{cnt.second, std::vector<double>{}})
+                 .first;
         it->second.s.reserve(reports.size());
       } else {
-        BM_CHECK_EQ(counter_stats[cnt.first].c.flags, cnt.second.flags);
+        BM_CHECK_EQ(it->second.c.flags, cnt.second.flags);
       }
     }
   }
