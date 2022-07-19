895f99a9fde3157bff755d135936564d3b9808f0
Dominic Hamon <dma+github@stripysock.com>
2022-07-19 18:05:05 +0100

    format the new header

--- include/benchmark/export.h
+++ include/benchmark/export.h
@@ -2,41 +2,40 @@
 #define BENCHMARK_EXPORT_H
 
 #ifdef BENCHMARK_STATIC_DEFINE
-#  define BENCHMARK_EXPORT
-#  define BENCHMARK_NO_EXPORT
+#define BENCHMARK_EXPORT
+#define BENCHMARK_NO_EXPORT
 #else
-#  ifndef BENCHMARK_EXPORT
-#    ifdef benchmark_EXPORTS
-        /* We are building this library */
-#      define BENCHMARK_EXPORT __attribute__((visibility("default")))
-#    else
-        /* We are using this library */
-#      define BENCHMARK_EXPORT __attribute__((visibility("default")))
-#    endif
-#  endif
+#ifndef BENCHMARK_EXPORT
+#ifdef benchmark_EXPORTS
+/* We are building this library */
+#define BENCHMARK_EXPORT __attribute__((visibility("default")))
+#else
+/* We are using this library */
+#define BENCHMARK_EXPORT __attribute__((visibility("default")))
+#endif
+#endif
 
-#  ifndef BENCHMARK_NO_EXPORT
-#    define BENCHMARK_NO_EXPORT __attribute__((visibility("hidden")))
-#  endif
+#ifndef BENCHMARK_NO_EXPORT
+#define BENCHMARK_NO_EXPORT __attribute__((visibility("hidden")))
+#endif
 #endif
 
 #ifndef BENCHMARK_DEPRECATED
-#  define BENCHMARK_DEPRECATED __attribute__ ((__deprecated__))
+#define BENCHMARK_DEPRECATED __attribute__((__deprecated__))
 #endif
 
 #ifndef BENCHMARK_DEPRECATED_EXPORT
-#  define BENCHMARK_DEPRECATED_EXPORT BENCHMARK_EXPORT BENCHMARK_DEPRECATED
+#define BENCHMARK_DEPRECATED_EXPORT BENCHMARK_EXPORT BENCHMARK_DEPRECATED
 #endif
 
 #ifndef BENCHMARK_DEPRECATED_NO_EXPORT
-#  define BENCHMARK_DEPRECATED_NO_EXPORT BENCHMARK_NO_EXPORT BENCHMARK_DEPRECATED
+#define BENCHMARK_DEPRECATED_NO_EXPORT BENCHMARK_NO_EXPORT BENCHMARK_DEPRECATED
 #endif
 
 #if 0 /* DEFINE_NO_DEPRECATED */
-#  ifndef BENCHMARK_NO_DEPRECATED
-#    define BENCHMARK_NO_DEPRECATED
-#  endif
+#ifndef BENCHMARK_NO_DEPRECATED
+#define BENCHMARK_NO_DEPRECATED
+#endif
 #endif
 
 #endif /* BENCHMARK_EXPORT_H */
-
