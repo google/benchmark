914d9c59cca42db08b0c507d2b360c3f15b1d0d9
Dominic Hamon <dma+github@stripysock.com>
2022-07-20 09:16:24 +0100

    support windows

--- include/benchmark/export.h
+++ include/benchmark/export.h
@@ -1,41 +1,47 @@
 #ifndef BENCHMARK_EXPORT_H
 #define BENCHMARK_EXPORT_H
 
+#ifdef BENCHMARK_OS_WINDOWS
+#define EXPORT_ATTR __declspec(dllexport)
+#define IMPORT_ATTR __declspec(dllimport)
+#define NO_EXPORT_ATTR
+#define DEPRECATED_ATTR __declspec(deprecated)
+#else  // BENCHMARK_OS_WINDOWS
+#define EXPORT_ATTR     __attribute__((visibility("default")))
+#define IMPORT_ATTR     __attribute__((visibility("default")))
+#define NO_EXPORT_ATTR  __attribute__((visibility("hidden")))
+#define DEPRECATE_ATTR __attribute__((__deprecated__))
+#endif  // BENCHMARK_OS_WINDOWS
+
 #ifdef BENCHMARK_STATIC_DEFINE
 #define BENCHMARK_EXPORT
 #define BENCHMARK_NO_EXPORT
-#else
+#else  // BENCHMARK_STATIC_DEFINE
 #ifndef BENCHMARK_EXPORT
 #ifdef benchmark_EXPORTS
 /* We are building this library */
-#define BENCHMARK_EXPORT __attribute__((visibility("default")))
-#else
+#define BENCHMARK_EXPORT EXPORT_ATTR
+#else  // benchmark_EXPORTS
 /* We are using this library */
-#define BENCHMARK_EXPORT __attribute__((visibility("default")))
-#endif
-#endif
+#define BENCHMARK_EXPORT IMPORT_ATTR
+#endif  // benchmark_EXPORTS
+#endif  // !BENCHMARK_EXPORT
 
 #ifndef BENCHMARK_NO_EXPORT
-#define BENCHMARK_NO_EXPORT __attribute__((visibility("hidden")))
-#endif
-#endif
+#define BENCHMARK_NO_EXPORT NO_EXPORT_ATTR
+#endif  // !BENCHMARK_NO_EXPORT
+#endif  // BENCHMARK_STATIC_DEFINE
 
 #ifndef BENCHMARK_DEPRECATED
-#define BENCHMARK_DEPRECATED __attribute__((__deprecated__))
-#endif
+#define BENCHMARK_DEPRECATED DEPRECATE_ATTR
+#endif  // BENCHMARK_DEPRECATED
 
 #ifndef BENCHMARK_DEPRECATED_EXPORT
 #define BENCHMARK_DEPRECATED_EXPORT BENCHMARK_EXPORT BENCHMARK_DEPRECATED
-#endif
+#endif  // BENCHMARK_DEPRECATED_EXPORT
 
 #ifndef BENCHMARK_DEPRECATED_NO_EXPORT
 #define BENCHMARK_DEPRECATED_NO_EXPORT BENCHMARK_NO_EXPORT BENCHMARK_DEPRECATED
-#endif
-
-#if 0 /* DEFINE_NO_DEPRECATED */
-#ifndef BENCHMARK_NO_DEPRECATED
-#define BENCHMARK_NO_DEPRECATED
-#endif
-#endif
+#endif  // BENCHMARK_DEPRECATED_EXPORT
 
 #endif /* BENCHMARK_EXPORT_H */
