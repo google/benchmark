b177433f3ee2513b1075140c723d73ab8901790f
Andrii Dushko <aodushko@gmail.com>
2023-03-30 21:17:14 +0000

    Guard NOMINMAX to prevent redefinition error (#1581) (#1582)
    Fixes #1581.

--- src/internal_macros.h
+++ src/internal_macros.h
@@ -44,7 +44,9 @@
   #define BENCHMARK_OS_WINDOWS 1
   // WINAPI_FAMILY_PARTITION is defined in winapifamily.h.
   // We include windows.h which implicitly includes winapifamily.h for compatibility.
-  #define NOMINMAX
+  #ifndef NOMINMAX
+    #define NOMINMAX
+  #endif
   #include <windows.h>
   #if defined(WINAPI_FAMILY_PARTITION)
     #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
