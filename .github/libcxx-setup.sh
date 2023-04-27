cde1b61b49ce662a0f5fdd8656335458e708b281
Dominic Hamon <dominic+github@google.com>
2023-04-27 17:12:13 +0100

    fix libcxx script paths

--- .github/libcxx-setup.sh
+++ .github/libcxx-setup.sh
@@ -11,7 +11,7 @@ if [ -z "$BUILD_32_BITS" ]; then
 fi
 
 ## Build and install libc++ (Use unstable ABI for better sanitizer coverage)
-cd ./llvm-project/runtimes/
+mkdir llvm-build && cd llvm-build
 cmake -DCMAKE_C_COMPILER=${CC}                  \
       -DCMAKE_CXX_COMPILER=${CXX}               \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo         \
@@ -20,10 +20,11 @@ cmake -DCMAKE_C_COMPILER=${CC}                  \
       -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER}  \
       -DLLVM_BUILD_32_BITS=${BUILD_32_BITS}     \
       -DLLVM_ENABLE_RUNTIMES='libcxx;libcxxabi;libunwind' \
-      -S llvm -B llvm-build -G "Unix Makefiles"
-make -C llvm-build -j3 cxx cxxabi unwind
-sudo make -C llvm-build install-cxx install-cxxabi install-unwind
-cd ../..
+      -G "Unix Makefiles" \
+      ../llvm-project/runtimes/
+make -j cxx cxxabi unwind
+sudo make install-cxx install-cxxabi install-unwind
+cd ..
 
 #sudo apt update
 #sudo apt -y install libc++-dev libc++abi-dev
