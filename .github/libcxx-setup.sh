7291fd77c2ebce4714d24d096fb3b98e01d6d488
Ashish Madhup <119279720+madhupashish@users.noreply.github.com>
2023-06-25 22:43:24 +0530

    Update libcxx-setup.sh
    Here are the improvements made to the code:
    
    1. Used `mkdir -p` to create the `llvm-build` directory. This ensures that the directory is created only if it doesn't exist.
    
    2. Added `:` before the `${BUILD_32_BITS:=OFF}` line to prevent the script from exiting if `BUILD_32_BITS` is already set.
    
    3. Changed `make -j cxx cxxabi unwind` to `make -j$(nproc) cxx cxxabi unwind` to utilize the number of available processors for parallel building. `$(nproc)` will provide the number of processors on most Unix-like systems.
    
    4. Enclosed the `cmake` command arguments in an array `cmake_args` to handle arguments with spaces properly.
    
    5. Modified the `cmake` command to use the array expansion `"${cmake_args[@]}"` to pass the arguments correctly to `cmake`. This ensures that arguments with spaces are handled correctly.
    
    These improvements enhance the script's readability, maintainability, and performance.

--- .github/libcxx-setup.sh
+++ .github/libcxx-setup.sh
@@ -2,25 +2,27 @@
 
 set -e
 
-# Checkout LLVM sources
+Checkout LLVM sources
 git clone --depth=1 https://github.com/llvm/llvm-project.git llvm-project
 
-## Setup libc++ options
-if [ -z "$BUILD_32_BITS" ]; then
-  export BUILD_32_BITS=OFF && echo disabling 32 bit build
-fi
+Set default values for optional variables
+: "${BUILD_32_BITS:=OFF}" # If BUILD_32_BITS is not set, default to OFF
 
-## Build and install libc++ (Use unstable ABI for better sanitizer coverage)
-mkdir llvm-build && cd llvm-build
-cmake -DCMAKE_C_COMPILER=${CC}                  \
-      -DCMAKE_CXX_COMPILER=${CXX}               \
-      -DCMAKE_BUILD_TYPE=RelWithDebInfo         \
-      -DCMAKE_INSTALL_PREFIX=/usr               \
-      -DLIBCXX_ABI_UNSTABLE=OFF                 \
-      -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER}  \
-      -DLLVM_BUILD_32_BITS=${BUILD_32_BITS}     \
-      -DLLVM_ENABLE_RUNTIMES='libcxx;libcxxabi;libunwind' \
-      -G "Unix Makefiles" \
-      ../llvm-project/runtimes/
-make -j cxx cxxabi unwind
+Build and install libc++
+mkdir -p llvm-build
+cd llvm-build
+cmake_args=(
+"-DCMAKE_C_COMPILER=${CC}"
+"-DCMAKE_CXX_COMPILER=${CXX}"
+"-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+"-DCMAKE_INSTALL_PREFIX=/usr"
+"-DLIBCXX_ABI_UNSTABLE=OFF"
+"-DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER}"
+"-DLLVM_BUILD_32_BITS=${BUILD_32_BITS}"
+"-DLLVM_ENABLE_RUNTIMES=libcxx;libcxxabi;libunwind"
+"-G" "Unix Makefiles"
+"../llvm-project/runtimes/"
+)
+cmake "${cmake_args[@]}"
+make -j$(nproc) cxx cxxabi unwind
 cd ..
