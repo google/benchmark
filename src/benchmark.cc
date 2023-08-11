69b85c5ac90e655c93ac7429229e6c467b7b1e7a
varshneydevansh <varshney.devansh614@gmail.com>
2023-08-11 12:50:16 +0530

    fix: added benchmark_counters_tabular for file

--- src/benchmark.cc
+++ src/benchmark.cc
@@ -574,8 +574,14 @@ size_t RunSpecifiedBenchmarks(BenchmarkReporter* display_reporter,
       std::exit(1);
     }
     if (!file_reporter) {
-      default_file_reporter = internal::CreateReporter(
-          FLAGS_benchmark_out_format, ConsoleReporter::OO_None);
+      if (FLAGS_benchmark_out_format == "console" &&
+          FLAGS_benchmark_counters_tabular) {
+        default_file_reporter = internal::CreateReporter(
+            FLAGS_benchmark_out_format, ConsoleReporter::OO_Tabular);
+      } else {
+        default_file_reporter = internal::CreateReporter(
+            FLAGS_benchmark_out_format, ConsoleReporter::OO_None);
+      }
       file_reporter = default_file_reporter.get();
     }
     file_reporter->SetOutputStream(&output_file);
