1cc5d6711f29eece9577e3d09c470d7825780dbe
Dominic Hamon <dominic+github@google.com>
2024-01-29 13:43:10 +0000

    clang format

--- src/benchmark.cc
+++ src/benchmark.cc
@@ -748,9 +748,7 @@ int InitializeStreams() {
 
 }  // end namespace internal
 
-std::string GetBenchmarkVersiom() {
-  return {BENCHMARK_VERSION};
-}
+std::string GetBenchmarkVersiom() { return {BENCHMARK_VERSION}; }
 
 void PrintDefaultHelp() {
   fprintf(stdout,
