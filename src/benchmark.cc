b66b20bb638f2b2a78da92f64fe799cdcf1afd3c
Doug Evans <xdje42@gmail.com>
2024-08-08 14:41:07 -0700

    Verify RegisterProfilerManager doesn't overwrite an existing registration
    Tested:
    Add a second registration to test/profiler_manager_test.cc and
    verify the test crashes as expected.

--- src/benchmark.cc
+++ src/benchmark.cc
@@ -663,6 +663,10 @@ void RegisterMemoryManager(MemoryManager* manager) {
 }
 
 void RegisterProfilerManager(ProfilerManager* manager) {
+  // Don't allow overwriting an existing manager.
+  if (manager != nullptr) {
+    BM_CHECK_EQ(internal::profiler_manager, nullptr);
+  }
   internal::profiler_manager = manager;
 }
 
