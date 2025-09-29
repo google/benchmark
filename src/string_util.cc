8afba0b13a8b7221f6677b764159240438cd02e6
Dominic Hamon <dominic+github@google.com>
2025-09-29 16:50:44 +0100

    remove done todo

--- src/string_util.cc
+++ src/string_util.cc
@@ -117,8 +117,8 @@ std::string StrFormatImp(const char* msg, va_list args) {
   va_list args_cp;
   va_copy(args_cp, args);
 
-  // TODO(ericwf): use std::array for first attempt to avoid one memory
-  // allocation guess what the size might be
+  // Use std::array for first attempt to avoid one memory allocation guess what
+  // the size might be
   std::array<char, 256> local_buff = {};
 
   // 2015-10-08: vsnprintf is used instead of snd::vsnprintf due to a limitation
